4d0a5c877cd7c11c1f199b2468affe7e9ad81214
Anthony Hauger <ahauger@dontdrivedirty.com>
2021-06-29 09:52:22 -0700

    fix: Changes to account handler

--- server/handlers/accounts.go
+++ server/handlers/accounts.go
@@ -1,19 +1,24 @@
 package handlers
 
-type AccountHandler struct{
-	Config
-	Client
+import (
+	"database/sql"
+	"fmt"
+)
+
+type AccountHandler struct {
+	Config models.AccountConnection
+	Client clients.Accountclient
 }
 
 func NewAccountHandler(aC clients.AccountClient) {
-	return &AccountHandler {
+	return &AccountHandler{
 		Client: nil,
 	}
 }
 
-func UpdateAccountHandler(config models.Config.AccountConnection) {
+func UpdateAccountHandler(config models.AccountConnection) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-						config.Host, config.Port, config.User, config.Password, config.DB)
+		config.Host, config.Port, config.User, config.Password, config.DB)
 	dbConnection, connectionErr := sql.Open("postgres", psqlInfo)
-	
+
 }
